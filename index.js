7b343d77ab97a8aaf03127db4ce08147b9db9105
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-17 16:35:34 -0500

    edited the h1 tags to be center and added flex to both containers.

---
+++ index.js
@@ -0,0 +1,6 @@
+const button1 = document.querySelector("#submit");
+const button2 = document.querySelector("page2")
+
+button1.addEventListener("click", function(){
+    submit.style.display="none";
+})
